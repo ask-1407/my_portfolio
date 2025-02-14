f34f60e3392fe76fd760382790e96f4b203f57cd
ask-1407 <a1s4u0k7a@gmail.com>
2025-02-14 09:07:17 +0900

    [p23-p27] アンパック推奨コード

---
+++ language/python/effective_python/01_pythonic.py
@@ -0,0 +1,54 @@
+# 項目6: インデックスではなく複数代入アンパックを使う
+
+"""
+アンパックを用いることで以下のメリット
+1.  行数が少なく見た目がスッキリする ※イテラブルなものなら可能
+2. 
+3.
+"""
+item = ("butter", "snack")
+first, second = item
+print(first, "and", second) # output: butter and snack
+
+
+# バブルソートの実装でもアンパックを用いると一行でスワップが実装可能。
+def bubble_sort(a):
+    for _ in range(len(a)):
+        for i in range(1, len(a)):
+            if a[i] < a[i-1]:
+                a[i-1], a[i] = a[i], a[i-1] # swap
+
+names = ["pretzels", "cattots", "argula", "bacon"]
+bubble_sort(names)
+print(names) # output: ['argula', 'bacon', 'cattots', 'pretzels']
+
+"""
+スワップの動作は代入演算子右側のa[i],a[i-1]がまず評価されて，値が一時的な無名のタプルに格納。
+次に代入演算子の左側のアンパック化ターンを使ってタプルの値を取り出してそれぞれの変数に代入する。
+最後に一時的な無名のタプルが廃棄される。
+"""
+
+
+# アンパックを用いないパターン. if内のswapが三行となっている。
+def bubble_sort(a):
+    for _ in range(len(a)):
+        for i in range(1, len(a)):
+            if a[i] < a[i-1]:
+                temp = a[i]
+                a[i] = a[i-1]
+                a[i-1] = temp
+
+
+
+# ジェネレータ式におけるターゲットリストにも使える。スナック食品リストのイテレーション
+snacks = [('bacon', 350), ('donut', 240), ('muffin', 190)]
+for rank, (name, calories) in enumerate(snacks, 1):
+    print(f"#{rank}: {name} has {calories} carolies")
+
+
+# anti-pattern. snacks変数の構造の深さごとにインデックスを使っているため文字が余計に必要
+for i in range(len(snacks)):
+    item = snacks[i]
+    name = item[0]
+    calories = item[1]
+    print(f"#{i+1}: {name} has {calories} carolies")
