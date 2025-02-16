b4a5a25f5fbf7dda6824255012450a2ca8aaf3b9
ask-1407 <a1s4u0k7a@gmail.com>
2025-02-16 14:57:36 +0900

    [p34-p39] 代入式の利用

--- language/python/effective_python/01_pythonic.py
+++ language/python/effective_python/01_pythonic.py
@@ -98,3 +98,178 @@ from itertools import zip_longest
 names.append('Rosalind')
 for name, count in zip_longest(names, count):
     print(f"{name}: {count}")
+
+
+# 項目9 forとwhileの後のelseは利用しない
+
+
+"""
+forの後のelseはループ終了直後に実行される。for文のなかにbreakがあるとelseがスキップされる。
+ループして何かを探す場合に使えはするものの，基本的にこれは利用しないほうが望ましい。
+ループのような単純構成要素は自明であるべきであり，for-elseの振る舞いは直感的でないため。
+"""
+
+# ２つの数が互いに素の数かどうかを調べるコード
+# パターン1：探している条件が見つかり次第早めにループを抜ける方法
+def coprime(a,b):
+    for i in range(2, min(a,b) + 1):
+        if a % i == 0 and b % i == 0:
+            return False
+    return True
+
+assert coprime(4, 9)
+assert not coprime(3, 6)
+
+# パターン2: ループして探していたものが見つかったかどうかを示す結果変数を使う
+def coprime_alternate(a, b):
+    is_coprime = True
+    for i in range(2, min(a,b) + 1 ):
+        if a % i == 0 and b % i == 0:
+            is_coprime = False
+            break
+    return is_coprime
+
+assert coprime_alternate(4, 9)
+assert not coprime_alternate(3, 6)
+
+# for-elseを用いた場合
+a = 4
+b = 9
+for i in range(2, min(a, b) + 1):
+    print('Testing', i)
+    if a % i == 0 and b % i == 0:
+        print('Not coprime')
+        break
+    else:
+        print('Coprime !')
+
+# 項目10 代入式で繰り返しを防ぐ
+"""
+ウォルラス演算子「:=」を使うとif文のような代入ができなかった箇所変数の代入ができる.
+ある範囲の行で同じ式や代入を複数回繰り返している場合は，代入式の利用を検討するべき。
+Pythonにはswitch/case文do/whileループはないが代入式を用いてそれらの機能を書くことができる。
+"""
+
+# lemonが少なくとも一つ以上あることを確認したい
+fresh_fruit = {
+    'apple': 10,
+    'banana': 8,
+    'lemon': 5,
+}
+
+def make_lemonade():
+    pass
+
+def out_of_stock():
+    pass
+
+class OutOfBananas(Exception):
+    pass
+
+
+# 以下のように書くことはできるが，変数countがif文の最初のブロックのみで使われておらず読みづらい。
+count = fresh_fruit.get('lemon', 0)
+if count:
+    make_lemonade(count)
+else:
+    out_of_stock()
+
+# ウォルラス演算子を利用することで明瞭さを向上できる。
+if (count := fresh_fruit.get('lemon', 0)) >= 0:
+    make_lemonade(count)
+else:
+    out_of_stock()
+
+# 以下のようにpieces変数をもとに関数をキックする際にもウォルラス演算子が有効
+# 使わない場合はこちら
+
+pieces = 0 # elseで定義する方法もあるが冒頭に初期化されるほうが好まれる。
+count = fresh_fruit.get('banana', 0)
+if count >= 2:
+    pieces = slice_bananas(count)
+
+try: 
+    smoothies = make_smoothies(pieces)
+except OutOfBananas:
+    out_of_stock()
+
+
+# 利用した場合. count変数の強調がなくなる。
+pieces = 0
+if (count := fresh_fruit.get('banana',0)) >= 2:
+    pieces = slice_bananas(count)
+
+try:
+    smoothies = make_smooothies(pieces)
+except OutOfBananas:
+    out_of_stock()
+
+"""
+Pythonにはswitch/caseがないので一般にはif/elif/elseを入れ子にすることで実現する
+例えばバナナスムージー，アップルサイダー，レモネードの優先度となっている場合
+"""
+
+# walrus演算子を利用しない書き方. 変数countへの代入が逐次走り読みにくい。
+count = fresh_fruit.get('banana', 0)
+if count >= 2:
+    pieces = slice_bananas(count)
+    to_enjoy = make_smoothies(pieces)
+else:
+    count = fresh_fruit.get('apple', 0)
+    if count >= 4:
+        to_enjoy = make_cider(count)
+    else:
+        count = fresh_fruit.get('lemon', 0)
+        if count:
+            to_enjoy = make_lemonade(count)
+        else:
+            to_enjoy = 'Nothing'
+
+# walrus演算子を利用する場合 元より5行少なくなり，入れ子が改善されている。
+if (count := fresh_fruit.get('banana', 0)) >= 2:
+    pieces = slice_bananas(count)
+    to_enjoy = make_smoothies(count)
+elif (count := fresh_fruit.get('appke', 0)) >= 4:
+    to_enjoy = make_cider()
+elif count := fresh_fruit.get('lemon', 0):
+    to_enjoy = make_lemonade(count)
+else:
+    to_enjoy = 'Nothing'
+
+"""
+do/whileのような内容に対してもwalrus演算子の利用で短くて読みやすいコードがかける。
+果物が届いたらジュースをビン詰めすることを考える。
+
+whileループによる実装例。fresh_fruit = pick_fruit() が2回登場する。
+"""
+def pick_fruit():
+    pass
+
+def make_juice(fruit, count):
+    pass
+
+bottles = []
+fresh_fruit = pick_fruit() # ループの最初の条件設定に利用
+while fresh_fruit:
+    for fruit, cunt in fresh_fruit.items():
+        batch = make_juice(fruit = fruit, count = count)
+        bottles.extend(batch)
+    fresh_fruit = pick_fruit() # ループ後に配達されたジュースのリストを補充するために使う
+
+# 条件付きbreakによる実装例。これでループを制御する。
+bottles = []
+while True: # ループ
+    fresh_fruit = pick_fruit()
+    if not fresh_fruit: # 付け足し
+        break
+    for fruit, cunt in fresh_fruit.items():
+        batch = make_juice(fruit = fruit, count = count)
+        bottles.extend(batch)
+
+# walrus演算子利用の例。変数fresh_fruitが再度さ移入されwhileループないで条件を毎回チェックする必要例をなくすことができる
+bottles = []
+while fresh_fruit := pick_fruit():
+    for fruit, count in fresh_fruit.items():
+        batch = make_juice(fruit = fruit, count = count)
+        bottles.extend(batch)
+
