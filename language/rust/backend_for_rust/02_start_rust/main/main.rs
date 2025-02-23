be641cd4725abd234799687a523d9f044e982dc8
ask-1407 <a1s4u0k7a@gmail.com>
2025-02-23 10:41:57 +0900

    [p86-p93] 変数の宣言とスコープ

--- language/rust/backend_for_rust/02_start_rust/main/main.rs
+++ language/rust/backend_for_rust/02_start_rust/main/main.rs
@@ -2,3 +2,23 @@ fn main() {
     println!("Hello, world!")
 }
 
+// スラッシュ2つでコメントとみなせる
+/*
+複数行はこんな感じで書く
+*/
+
+
+// let 変数名＝値; にて変数を宣言する
+let char_a = 'a'; //char型
+let str_a = "a"; //&str型
+let string_b = "b.to_string()"; //String型
+
+// dbg!(変数名);を実行すると変数の値が出力される
+let one =1;
+dbg!(one);
+
+// 以下のように書くと変数oneのスコープは関数内のみとなる。
+fn main() {
+    let one =1;
+    dbg!(one);
+}
