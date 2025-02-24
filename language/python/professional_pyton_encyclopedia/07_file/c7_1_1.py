a355a0f5770ace971da30657972f55629b6f6c91
ask-1407 <a1s4u0k7a@gmail.com>
2025-02-24 22:59:43 +0900

    [p227-p228]　ファイルの追記・上書き

--- language/python/professional_pyton_encyclopedia/07_file/c7_1_1.py
+++ language/python/professional_pyton_encyclopedia/07_file/c7_1_1.py
@@ -2,3 +2,13 @@
 f = open('test.txt','w')
 f.write('test')
 f.close()
+
+# もともとテキストファイルがある場合、上書きされる
+f = open('test.txt','w')
+f.write('Test')
+f.close()
+
+# 追記の場合、open()メソッドの引数を'a'にする。
+f = open('test.txt','a+')
+f.write('Test')
+f.close()
