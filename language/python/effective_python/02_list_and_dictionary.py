63efd45c661cde7b9e1ac53f85634dc8bed98ffd
ask-1407 <a1s4u0k7a@gmail.com>
2025-03-02 23:21:06 +0900

    [p69] defaultdictの実装例

--- language/python/effective_python/02_list_and_dictionary.py
+++ language/python/effective_python/02_list_and_dictionary.py
@@ -473,4 +473,19 @@ visits.add('Russia','Yekaterinburg')
 visits.add('Tanzania','Zanzibar')
 print(visits.data) # >>> {'Russia': {'Yekaterinburg'}, 'Tanzania': {'Zanzibar'}}
 
-# 
+# collections.defaultdictにはキーが存在しない場合のデフォルト値を自動格納してこのユースケースを単純化する機能がある。
+from collections import defaultdict
+class Visits:
+    def __init__(self):
+        self.data = defaultdict(set)
+    
+    def add(self, country, city):
+        self.data[country].add(city) # addの実装が短くなる。
+
+visits = Visits()
+visits.add('England', 'Bath')
+visits.add('England', 'London')
+print(visits.data) # >>> defaultdict(<class 'set'>, {'England': {'Bath', 'London'}})
+
+
+
