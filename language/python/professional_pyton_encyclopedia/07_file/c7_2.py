08c7dd8c87667115a7e035c26f0488ddd821aa58
ask-1407 <a1s4u0k7a@gmail.com>
2025-03-03 23:02:26 +0900

    [p237-p242] 各種ファイル操作コマンド

--- language/python/professional_pyton_encyclopedia/07_file/c7_2.py
+++ language/python/professional_pyton_encyclopedia/07_file/c7_2.py
@@ -27,5 +27,54 @@ Have a good day
 # CSVファイルへの書き込み
 import csv
 
+with open('test.csv','w', newline='') as csv_file: # windowsでは改行文字コードが\r\nとなるため読み込み時に2行改行される。newline=''を入れると解消される。
+    fieldnames = ['Name', 'Count']
+    writer = csv.DictWriter(csv_file, fieldnames)
+    writer.writeheader()
 
+# データ部分を書き込むにはwriterowメソッドに辞書型のデータを渡す。
+with open('test.csv','w', newline='') as csv_file:
+    fieldnames = ['Name', 'Count']
+    writer = csv.DictWriter(csv_file, fieldnames)
+    writer.writeheader()
+    writer.writerow({'Name': 'A', 'Count': 1})
+    writer.writerow({'Name': 'B', 'Count': 2})
 
+# DictReader関数を引数にcsvファイルを渡して読み込んでいく
+with open('test.csv','r', newline='') as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        print(row['Name'], row['Count'])
+
+# 標準ライブラリosを使うと様々なファイル操作を実行できる
+import os
+print(os.path.exists('test.txt')) # 存在するならTrueを返す
+
+# ファイルかどうかを確かめるにはisfileを，ディレクトリかどうかはisdirをつかう
+print(os.path.isfile('test.txt'))
+print(os.path.isdir('design')) 
+
+# その他の操作
+os.rename('test.txt', 'renamed.txt') # ファイル名の変更
+os.symlink('renamed.txt','symlink.txt') # シンボリックリンクの作成
+os.mkdir('test_dir') # ディレクトリ作成
+os.rmdir('test_dir') # ディレクトリ削除 ※ディレクトリが空の時に利用できる。
+
+# pathlibのtouchを使うと中身が空を作成できる。
+import pathlib
+
+pathlib.Path('empty.txt').touch()
+os.remove('empty.txt') # ファイル削除
+print(os.listdir('test_dir')) # 配下のディレクトリ表示
+
+# glob関数でファイルの列挙が可能。
+import glob
+pathlib.Path('test_dir/test_dir2/empty.txt').touch()
+print(glob.glob('test_dir/test_dir2/*'))
+
+# shutil.copy()を使うとファイルのコピーができる
+import shutil
+shutil.copy('test_dir/test_dir2/empty.txt','test_dir/test_dir2/empty2.txt')
+
+# shutil.rmtreeでディレクトリを削除できる(中身含む)
+shutil.rmtree('test_dir')
