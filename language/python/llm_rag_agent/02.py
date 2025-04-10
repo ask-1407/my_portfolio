8ac2e5da7d06cd8412db40fc951d2ce873e3a0c4
ask-1407 <a1s4u0k7a@gmail.com>
2025-04-10 16:41:28 +0900

    [2.6] Function Callingの実装

--- language/python/llm_rag_agent/02.py
+++ language/python/llm_rag_agent/02.py
@@ -91,5 +91,92 @@ print(response.choices[0].message.content)
 # 2.6 Function Calling
 """
 - 利用可能な関数とLLMに伝えておいてLLMに関数を使いたいという判断を指せる機能
-- LLMが関数を実行するわけではない
+- LLMが関数を実行するわけではなく，関数の実行はPython等を使って実装者で実行。
+"""
+
+# 天気を取得する関数を用意
+import json
+
+def get_current_weather(location, unit="fahrenheit"):
+    if "tokyo" in location.lowet():
+        return json.dumps({"location": "Tokyo", "unit": unit, "temperature": 20})
+
+    elif "new york" in location.lower():
+        return json.dumps({"location": "new york", "unit": unit, "temperature": 72})
+        
+    elif "paris" in location.lower():
+        return json.dumps({"location": "paris", "unit": unit, "temperature": 22})
+
+# LLMが利用できる関数の一覧を定義
+# NOTE functionsというパラメータがリリース当初はあるが，現時点では非推奨。
+tools = [
+     {
+         "type": "function",
+         "function": {
+             "name": "get_current_weather",
+             "description": "Get the current weather in a given location",
+             "parameters": {
+                 "type": "object",
+                 "properties": {
+                     "location": {
+                         "type": "string",
+                         "description": "The city and state, e.g. San Francisco, CA",
+                     },
+                     "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+                 },
+                 "required": ["location"],
+             },
+         },
+     }
+ ]
+
+from openai import OpenAI
+
+client = OpenAI()
+messages = [
+    {"role": "user", "content": "今の東京の天気は？"}
+]
+
+response = client.chat.completions.create(
+    model="gpt-4o",
+    messages = messages,
+    tools = tools,
+)
+print(response.to_json(indent=2))
+
+"""
+{
+  "id": "chatcmpl-9jj2wv14w0fYHr6CUAfe0MW2zL7nT",
+  "choices": [
+    {
+      "finish_reason": "tool_calls",
+      "index": 0,
+      "logprobs": null,
+      "message": {
+        "content": null,  ← 今までの実行例ではLLMが生成したテキストはここに含まれていた
+        "role": "assistant",
+        "tool_calls": [
+          {
+            "id": "call_if3ni88ahchw2egXtxZNxA7",
+            "function": {
+              "arguments": "{\"location\":\"Tokyo\"}", 
+              "name": "get_current_weather"
+            },
+            "type": "function"
+          }
+        ]
+      }
+    }
+  ],
+  "created": 1720684945,
+  "model": "gpt-4o-2024-05-13",
+  "object": "chat.completion",
+  "system_fingerprint": "fp_298125635f",
+  "usage": {
+    "completion_tokens": 15,
+    "prompt_tokens": 81,
+    "total_tokens": 96
+  }
+}
+
 """
