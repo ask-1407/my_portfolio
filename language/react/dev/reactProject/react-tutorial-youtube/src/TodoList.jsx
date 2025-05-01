d708f61acb2d4858c399144d0ec5fa17b9a19f0a
ask-1407 <a1s4u0k7a@gmail.com>
2025-05-01 15:31:11 +0900

    [update] コンポーネント作成

--- language/react/dev/reactProject/react-tutorial-youtube/src/TodoList.jsx
+++ language/react/dev/reactProject/react-tutorial-youtube/src/TodoList.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
+import Todo from "./Todo";
 
 const TodoList = ({todos}) => {
-  return (
-    <div>
-      {todos} 
-    </div>
-  )
-}
+  return todos.map((todo) => <Todo todo={todo}/>); //map()でtodoをコンポーネントに渡す
+
+};
 
 export default TodoList
