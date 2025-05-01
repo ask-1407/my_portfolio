32dac4b799e3f4fccc4b7b991c5f5a2b78ceec8d
ask-1407 <a1s4u0k7a@gmail.com>
2025-05-01 16:52:51 +0900

    [update] handleAddTodoコンポーネント作成

--- language/react/dev/reactProject/react-tutorial-youtube/src/App.js
+++ language/react/dev/reactProject/react-tutorial-youtube/src/App.js
@@ -1,13 +1,29 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import TodoList from "./TodoList";
 
 function App() {
-  const [todos, setTodos] = useState([{id:1, name:"Todo1",completed: false}]); //constの変数を管理
+  const [todos, setTodos] = useState([
+    {id:1, name:"Todo1",completed: false},
+  ]); //constの変数を管理
+  
+  const todoNameRef = useRef(); //要素を取得する
+
+  const handleAddTodo = () =>{
+    //タスクを追加する
+    const name = todoNameRef.current.value;
+    
+    //これがTodosの更新を担う。
+    setTodos((prevTodos)=> {
+      return [...prevTodos, {id: "1", name: name, completed: false}]; //前のタスクに対し新しいタスクオブジェクトを追加
+    })
+    todoNameRef.current.value=null;
+  };
+
   return (
     <div>
       <TodoList todos={todos} />
-      <input type = "text"/>
-      <button>タスクを追加</button>
+      <input type = "text" ref = {todoNameRef}/>
+      <button onClick={handleAddTodo}>タスクを追加</button>
       <button>完了したタスクの削除</button>
       <div>残りのタスク:0</div>
     </div>
