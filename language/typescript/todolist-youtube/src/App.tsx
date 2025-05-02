b4b90975f583327f3c6a1c147edb33053fa1b9f4
ask-1407 <a1s4u0k7a@gmail.com>
2025-05-02 16:36:29 +0900

    [update] フォーム入力の更新

--- language/typescript/todolist-youtube/src/App.tsx
+++ language/typescript/todolist-youtube/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css'
 import { useState } from 'react';
 
@@ -11,22 +10,44 @@ function App() {
     inputValue: string;
     id: number;
     checked: boolean;
-  }
+  };
 
   // handleChangedは引数eを受け取る。なお型を指定しないとErrorをはく。※TypeScript特有
   const handleChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
     //console.log(e.target.value);
     setInputValue(e.target.value)
-  }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // ページのリロードを防ぐ
+
+    // 新しいTodoを作成
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length, //　本来ならUnique IDを指定する
+      checked: false
+    };
+
+    setTodos([newTodo, ...todos]); //スプレッド構文で展開しnewTodoを追加。
+    setInputValue("");
+
+  };
 
   return (
     <div className="App">
       <div>
         <h2> Todoリスト with TypeScript</h2>
-        <form onSubmit = {() => {}}>
+        {/* フォーム入力欄 */}
+        <form onSubmit = {(e) => {handleSubmit(e)}}>
          <input type = "text" onChange={(e) => {handleChanged(e)}} className="inputText"/>
          <input type = "submit" value="作成" className='submitButton'/>
         </form>
+        {/* ToDoリストの表示 */}
+         <ul>
+           {todos.map( (todo) => (
+            <li>  {todo.inputValue} </li>
+           ))}
+         </ul>
       </div>
     </div>
   );
