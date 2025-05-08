a08ff015dbe0b6f214089fb22c97ce1bfb412ac3
ask-1407 <a1s4u0k7a@gmail.com>
2025-05-08 18:57:49 +0900

    [update] varを使わない理由

--- language/typescript/basics/01_variable declaration.ts
+++ language/typescript/basics/01_variable declaration.ts
@@ -12,7 +12,31 @@ const obj = { a: 1 };
 obj = { a: 2 }; // 再代入は不可
 obj.a = 2; // プロパティの変更はできる
 
+// 宣言時に型を指定することもできる。
+const num: number = 123;
+
 // 使い分け：基本的にはconstによって再代入を禁止し，意図しない書き換えを防ぐのがよい。
+
+/*
+varは以下の理由から使用しない。
+- 同じ変数名を宣言すると後から宣言された変数が有効となる。
+- グローバル変数として定義されたとき，windowオブジェクトのプロパティとして定義されるため既存のプロパティを上書きする危険性がある。
+- 変数巻き上げによる予期せぬ動作
+*/
+
+// varならエラーはでない。xは宣言されているが値は代入されていない。
+console.log(x); // undefined
+var x = 10;
+console.log(x); // 10
+
+// letやconstなら参照エラー
+console.log(y); // ReferenceError: Cannot access 'y' before initialization
+let y = 20;
+console.log(y);
+
+console.log(z); // ReferenceError: Cannot access 'z' before initialization
+const z = 30;
+console.log(z);
 
 // readonlyプロパティをつけると読み取り専用になる。
 let obj: {
@@ -78,3 +102,48 @@ const writableNumbers: number[] = readonlyNumbers;// The type 'readonly number[]
 const readonlyNumbers: readonly number[] = [1, 2, 3];
 const writableNumbers: number[] = readonlyNumbers as number[];
 
+//constアサーション：as const
+
+// 変数宣言時にas constをつけるとreadonlyにしたうえでリテラル型にする。
+const arrasyAsConst = [1, 2, 3] as const;
+
+const pokemon = {
+    name: "pikachu",
+    no: 25,
+    genre: "mouse pokemon",
+    height: 0.4,
+    weight: 6.0,
+} as const;
+
+/*
+[違い] 
+- readonlyはプロパティごとにつけることができる，
+‐ const assertionは再帰的にreadonlyにできる。
+*/
+type Country = {
+    name: string;
+    capitalCity: string;
+};
+   
+type Continent = {
+    readonly name: string;
+    readonly canada: Country;
+    readonly us: Country;
+    readonly mexico: Country;
+};
+   
+const america: Continent = {
+    name: "North American Continent",
+    canada: {
+      name: "Republic of Canada",
+      capitalCity: "Ottawa",
+    },
+    us: {
+      name: "United States of America",
+      capitalCity: "Washington, D.C.",
+    },
+    mexico: {
+      name: "United Mexican States",
+      capitalCity: "Mexico City",
+    },
+  };
