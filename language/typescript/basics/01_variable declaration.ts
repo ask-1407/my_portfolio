e09f606b58058bfc4362dcb1f6f5237d476d7e30
ask-1407 <a1s4u0k7a@gmail.com>
2025-05-08 17:54:48 +0900

    [update] 配列における読み取り変更

--- language/typescript/basics/01_variable declaration.ts
+++ language/typescript/basics/01_variable declaration.ts
@@ -47,3 +47,34 @@ let obj: Readonly<{
 const arr = [1, 2];
 arr = [3, 4]; // 再代入は不可
 arr.push(3); // 要素の変更はできる
+
+/*
+配列も読み取り専用として型注釈できる。以下2通りの方法がある。
+1: readonlyキーワードを使う方法
+2: ReadonlyArray<T>を使う方法
+両者に違いはないので書き手の好みで選択する。
+*/
+
+//1: readonly number[]と書くと変数の型がnumberの読み取り専用になる。
+const nums: readonly number[] = [1,2,3];
+
+//2: ReadonlyArray<T> のような書き方でも読み取り専用の配列型になる。
+const nums: ReadonlyArray<number> = [1, 2, 3];
+ 
+/*
+[特徴]
+読み取り型配列では，pushやpopのような破壊的操作がコンパイル時にない扱いとなるため
+呼び出そうとするとＴypeScriptコンパイラーに警告される。
+*/
+const nums: readonly number[] = [1, 2, 3];
+nums.push(4); //Property 'push' does not exist on type 'readonly number[]'.
+
+
+// 代入はコンパイルエラー
+const readonlyNumbers: readonly number[] = [1, 2, 3];
+const writableNumbers: number[] = readonlyNumbers;// The type 'readonly number[]' is 'readonly' and cannot be assigned to the mutable type 'number[]
+
+// どうしても読み取り専用配列を普通の配列に代入したいときは、型アサーション(type assertion)を使う
+const readonlyNumbers: readonly number[] = [1, 2, 3];
+const writableNumbers: number[] = readonlyNumbers as number[];
+
