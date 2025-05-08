4b8ebe4316f2c380333e862acaec56151b13d550
ask-1407 <a1s4u0k7a@gmail.com>
2025-05-08 17:37:59 +0900

    [update] readonlyと配列の変更

--- language/typescript/basics/01_variable declaration.ts
+++ language/typescript/basics/01_variable declaration.ts
@@ -14,5 +14,36 @@ obj.a = 2; // プロパティの変更はできる
 
 // 使い分け：基本的にはconstによって再代入を禁止し，意図しない書き換えを防ぐのがよい。
 
+// readonlyプロパティをつけると読み取り専用になる。
+let obj: {
+    readonly foo: number;
+};
+obj = { foo: 1 };
+obj.foo = 2;
 
+// 再帰的には適用されないので子や孫にもreadonlyをつける。
+let obj: {
+    readonly foo: {
+      readonly bar: number;
+    };
+  };
 
+// ユーティリティ型のRadonlyを使えば一括で変更できる。
+let obj: Readonly<{
+    a: number;
+    b: number;
+    c: number;
+    d: number;
+    e: number;
+    f: number;
+  }>;
+
+/*
+読み取り専用指定を受けたプロパティがチェックを受けるのはコンパイル時だけであり
+コンパイルされた後のJavaScriptとしては、readonlyがついていたプロパティも代入可能になる。
+*/
+
+// 配列の場合：変数自体の再代入はできません。しかし、配列要素は変更できます
+const arr = [1, 2];
+arr = [3, 4]; // 再代入は不可
+arr.push(3); // 要素の変更はできる
